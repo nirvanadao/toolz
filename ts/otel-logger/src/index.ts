9c806bd8172981cd6ec327e41f128df0c5ae19d3
nirvana-sid <sid@nirvana.finance>
2026-02-04 13:31:53 -0600

    simple logger/obs

--- ts/otel-logger/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @nirvana-tools/otel-logger
- *
- * One function. Auto-detects service vs job.
- */
-
-export { init, getLogger, InitConfig } from "./init"
-export type { Logger } from "winston"
